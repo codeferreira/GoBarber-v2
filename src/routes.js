d65869c2a6713a4bb5809657ce84aada5aa73611
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-08-15 23:24:40 -0300

    Add user and session routes
    - POST /users
    - PUT /users
    - POST /sessions

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,13 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/User';
+import SessionController from './app/controllers/Session';
+
 const routes = new Router();
 
-routes.get('/', (request, response) => response.send('Hello World!'));
+routes.post('/users', UserController.store);
+routes.put('/users', UserController.update);
+
+routes.post('/sessions', SessionController.store);
 
 export default routes;
