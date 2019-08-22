9e002c2ea2e10a853dcc63678dce6ee0fdc3fe85
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-08-22 07:38:27 -0300

    feat: Create pagination to Appointments list

--- src/app/controllers/Appointment.js
+++ src/app/controllers/Appointment.js
@@ -6,10 +6,14 @@ import Appointment from '../models/Appointment';
 
 class AppointmentController {
   async index(request, response) {
+    const { page = 1 } = request.query;
+
     const appointments = await Appointment.findAll({
       where: { user_id: request.userId, canceled_at: null },
       order: ['date'],
       attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
       include: [
         {
           model: User,
