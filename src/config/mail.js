e6169528a368635ba8df7df73d85c888e2a5477d
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-09-04 19:05:35 -0300

    feat: Config nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '1b256757490444',
+    pass: '37a43d20533940',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
