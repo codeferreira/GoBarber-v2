3463bd013ff17370a67e632f8c108f30c272a6af
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-11-20 15:42:53 -0300

    chore: SignUp Layout

--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import {Form, Input} from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('Name is required'),
+  email: Yup.string()
+    .email('Invalid e-mail')
+    .required('E-mail is required'),
+  password: Yup.string()
+    .min(6, 'Minimum 6 characters')
+    .required('Password is required'),
+});
 
 export default function SignUp() {
+  function handleSubmit(data) {}
+
   return (
-    <div>
-      <h1>SignUp</h1>
-    </div>
+    <>
+      <img src={logo} alt="Gobarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Your full name" />
+        <Input type="email" name="email" placeholder="Your e-mail" />
+        <Input type="password" name="password" placeholder="Your password" />
+
+        <button type="submit">Sign Up</button>
+        <Link to="/">Already have an account</Link>
+      </Form>
+    </>
   );
 }
