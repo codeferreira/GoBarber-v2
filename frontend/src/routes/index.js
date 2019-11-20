0b63e7f0f91e7d3fbc28b7d9eb637c0633e11d93
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-11-20 15:44:02 -0300

    fix: Change to root import & add isPrivate prop to routes

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -3,10 +3,11 @@ import {Switch} from 'react-router-dom';
 
 import Route from './Route';
 
-import SignIn from '../pages/SignIn';
-import SignUp from '../pages/SignUp';
-import Dashboard from '../pages/Dashboard';
-import Profile from '../pages/Profile';
+import SignIn from '~/pages/SignIn';
+import SignUp from '~/pages/SignUp';
+
+import Dashboard from '~/pages/Dashboard';
+import Profile from '~/pages/Profile';
 
 export default function Routes() {
   return (
@@ -14,8 +15,8 @@ export default function Routes() {
       <Route path="/" exact component={SignIn} />
       <Route path="/register" component={SignUp} />
 
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/profile" component={Profile} />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
     </Switch>
   );
 }
