de182513e1f52452f6e1612d8b70681586ef4d66
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-11-20 15:44:20 -0300

    chore: Dynamic check if is signed

--- frontend/src/routes/Route.js
+++ frontend/src/routes/Route.js
@@ -5,12 +5,14 @@ import {Route, Redirect} from 'react-router-dom';
 import AuthLayout from '~/pages/_layouts/auth';
 import DefaultLayout from '~/pages/_layouts/default';
 
+import store from '~/store';
+
 export default function RouteWrapper({
   component: Component,
-  isPrivate = false,
+  isPrivate,
   ...rest
 }) {
-  const signed = false;
+  const {signed} = store.getState().auth;
 
   if (!signed && isPrivate) {
     return <Redirect to="/" />;
@@ -23,14 +25,16 @@ export default function RouteWrapper({
   const Layout = signed ? DefaultLayout : AuthLayout;
 
   return (
-    <Route
-      {...rest}
-      render={props => (
-        <Layout>
-          <Component {...props} />
-        </Layout>
-      )}
-    />
+    <>
+      <Route
+        {...rest}
+        render={props => (
+          <Layout>
+            <Component {...props} />
+          </Layout>
+        )}
+      />
+    </>
   );
 }
 
