746fb12ef9f2fe6c7a55732ae33dc97263c3822a
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-11-20 15:42:43 -0300

    feat: SignIn layout

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import {useDispatch} from 'react-redux';
+import {Link} from 'react-router-dom';
+import {Form, Input} from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import {signInRequest} from '~/store/modules/auth/actions';
+
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Invalid e-mail')
+    .required('E-mail is required'),
+  password: Yup.string().required('Password is required'),
+});
 
 export default function SignIn() {
+  const dispatch = useDispatch();
+
+  function handleSubmit({email, password}) {
+    dispatch(signInRequest(email, password));
+  }
+
   return (
-    <div>
-      <h1>SignIn</h1>
-    </div>
+    <>
+      <img src={logo} alt="Gobarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input type="email" name="email" placeholder="Your e-mail" />
+        <Input type="password" name="password" placeholder="Your password" />
+
+        <button type="submit">Sign In</button>
+        <Link to="/register">SignUp for Free</Link>
+      </Form>
+    </>
   );
 }
