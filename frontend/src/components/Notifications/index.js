73834883f2e1c852928a02aab25992a248eb65aa
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-11-22 00:02:36 -0300

    chore: Add notifications

---
+++ frontend/src/components/Notifications/index.js
@@ -0,0 +1,81 @@
+import React, {useState, useEffect, useMemo} from 'react';
+import {parseISO, formatDistance} from 'date-fns';
+import en from 'date-fns/locale/en-US';
+
+import {MdNotifications} from 'react-icons/md';
+
+import {
+  Container,
+  Badge,
+  NotificationList,
+  Scroll,
+  Notification,
+} from './styles';
+import api from '~/services/api';
+
+export default function Notifications() {
+  const [visible, isVisible] = useState(false);
+  const [notifications, setNotifications] = useState([]);
+
+  const hasUnread = useMemo(
+    () => !!notifications.find(notification => notification.read === false),
+    [notifications],
+  );
+
+  useEffect(() => {
+    async function loadNotifications() {
+      const response = await api.get('notifications');
+
+      const data = response.data.map(notification => ({
+        ...notification,
+        timeDistance: formatDistance(
+          parseISO(notification.createdAt),
+          new Date(),
+          {addSuffix: true, locale: en},
+        ),
+      }));
+
+      setNotifications(data);
+    }
+
+    loadNotifications();
+  }, []);
+
+  function handleToogleVisible() {
+    isVisible(!visible);
+  }
+
+  async function handleMarkAsRead(id) {
+    await api.put(`notifications/${id}`);
+
+    setNotifications(
+      notifications.map(notification =>
+        notification._id === id ? {...notification, read: true} : notification,
+      ),
+    );
+  }
+
+  return (
+    <Container>
+      <Badge onClick={handleToogleVisible} hasUnread={hasUnread}>
+        <MdNotifications color="#7159c1" size={20} />
+      </Badge>
+
+      <NotificationList visible={visible}>
+        <Scroll>
+          {notifications.map(notification => (
+            <Notification key={notification._id} unread={!notification.read}>
+              <p>{notification.content}</p>
+              <time>{notification.timeDistance}</time>
+              <button
+                type="button"
+                onClick={() => handleMarkAsRead(notification._id)}>
+                Mark as read
+              </button>
+            </Notification>
+          ))}
+        </Scroll>
+      </NotificationList>
+    </Container>
+  );
+}
