4ffc16419db0c8251b064dc1475b6f14c3b0911f
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-11-24 10:35:08 -0300

    feat: Add user profile page

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,11 +1,54 @@
 import React from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {Form, Input} from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import {signOut} from '~/store/modules/auth/actions';
+import {updateProfileRequest} from '~/store/modules/user/actions';
+
+import AvatarInput from './AvatarInput';
+
+import {Container} from './styles';
 
 export default function Profile() {
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+
   return (
-    <div>
-      <h1>Profile</h1>
-    </div>
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+
+        <Input name="name" placeholder="Full Name" />
+        <Input name="email" type="email" placeholder="Your e-mail" />
+
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Your current password"
+        />
+        <Input type="password" name="password" placeholder="New password" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm new password"
+        />
+
+        <button type="submit">Update profile</button>
+      </Form>
+
+      <button type="button" onClick={handleSignOut}>
+        Log out
+      </button>
+    </Container>
   );
 }
