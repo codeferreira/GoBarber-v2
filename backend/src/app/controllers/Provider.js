d8e513707f9825e5fc9d21c16bb3a648fc3e5697
José JC. Carlos <dev.josejunior@gmail.com>
2019-11-26 15:35:55 -0300

    feat: Add providers cache list

--- backend/src/app/controllers/Provider.js
+++ backend/src/app/controllers/Provider.js
@@ -1,8 +1,16 @@
 import User from '../models/User';
 import File from '../models/File';
 
+import Cache from '../../lib/Cache';
+
 class ProviderController {
   async index(request, response) {
+    const cached = await Cache.get('providers');
+
+    if (cached) {
+      return response.json(cached);
+    }
+
     const providers = await User.findAll({
       where: { provider: true },
       attributes: ['id', 'name', 'email', 'avatar_id'],
@@ -14,6 +22,8 @@ class ProviderController {
     if (!providers.length) {
       return response.json({ error: 'There is no provider.' });
     }
+
+    await Cache.set('providers', providers);
 
     return response.json(providers);
   }
